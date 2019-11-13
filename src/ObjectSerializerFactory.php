aca89efbfa155e1767cc858987f6e1a8756bb050
Lukasz93P <lukasz9.3@interia.pl>
2019-11-13 21:53:00 +0100

    tests

--- src/ObjectSerializerFactory.php
+++ src/ObjectSerializerFactory.php
@@ -4,6 +4,10 @@
 namespace Lukasz93P\objectSerializer;
 
 
+use Doctrine\Common\Annotations\AnnotationRegistry;
+
+AnnotationRegistry::registerLoader('class_exists');
+
 final class ObjectSerializerFactory
 {
     public static function create(array $objectIdentificationKeysToClassNamesMapping): ObjectSerializer
